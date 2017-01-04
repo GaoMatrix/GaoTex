bdb0f040728ee79b3f389f9b2ebdde6a68548860
GaoMatrix <gcquan08@gmail.com>
2017-01-04 21:52:30 +0800

    头像上传

--- src/com/gao/nsfw/user/action/UserAction.java
+++ src/com/gao/nsfw/user/action/UserAction.java
@@ -1,8 +1,14 @@
 package com.gao.nsfw.user.action;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Resource;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
 
 import com.gao.nsfw.user.entity.User;
 import com.gao.nsfw.user.service.UserService;
@@ -15,6 +21,9 @@ public class UserAction extends ActionSupport{
 	private User user;
 	private List<User> userList;
 	private String[] selectedRow;
+	private File headImg;
+	private String headImgContentType;
+	private String headImgFileName;
 	
 	// 列表页面
 	public String listUI() {
@@ -30,6 +39,23 @@ public class UserAction extends ActionSupport{
 	// 保存新增
 	public String add() {
 		if (null != user) {
+			// 处理头像
+			if (headImg != null) {
+				//1、保存头像到upload/user
+				//获取保存路径的绝对地址
+				String filePath = ServletActionContext.getServletContext()
+						.getRealPath("upload/user");
+				String fileName = UUID.randomUUID().toString() 
+						+ headImgFileName.substring(headImgFileName.lastIndexOf("."));
+				// 复制文件
+				try {
+					FileUtils.copyFile(headImg, new File(filePath, fileName));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				//2、设置用户头像路径
+				user.setHeadImg("user/" + fileName);// user/123.jpg
+			}
 			userService.save(user);
 		}
 		return "list";
@@ -43,6 +69,23 @@ public class UserAction extends ActionSupport{
 	// 保存编辑
 	public String edit() {
 		if (null != user) {
+			// 处理头像
+			if (headImg != null) {
+				//1、保存头像到upload/user
+				//获取保存路径的绝对地址
+				String filePath = ServletActionContext.getServletContext()
+						.getRealPath("upload/user");
+				String fileName = UUID.randomUUID().toString() 
+						+ headImgFileName.substring(headImgFileName.lastIndexOf("."));
+				// 复制文件
+				try {
+					FileUtils.copyFile(headImg, new File(filePath, fileName));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				//2、设置用户头像路径
+				user.setHeadImg("user/" + fileName);// user/123.jpg
+			}
 			userService.update(user);
 		}
 		return "list";
@@ -89,6 +132,32 @@ public class UserAction extends ActionSupport{
 	public void setSelectedRow(String[] selectedRow) {
 		this.selectedRow = selectedRow;
 	}
+
+	public File getHeadImg() {
+		return headImg;
+	}
+
+	public void setHeadImg(File headImg) {
+		this.headImg = headImg;
+	}
+
+	public String getHeadImgContentType() {
+		return headImgContentType;
+	}
+
+	public void setHeadImgContentType(String headImgContentType) {
+		this.headImgContentType = headImgContentType;
+	}
+
+	public String getHeadImgFileName() {
+		return headImgFileName;
+	}
+
+	public void setHeadImgFileName(String headImgFileName) {
+		this.headImgFileName = headImgFileName;
+	}
+
+	
 	
 	
 }
